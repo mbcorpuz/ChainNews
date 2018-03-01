59f69b24151df8d2c5a89bda954b10b9140c08a0
Mark Anthony Corpuz <mbcorpuz7@gmail.com>
2018-03-01 15:34:48 -0800

    setup Xcode project

---
+++ ChainNews/ChainNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChainNews
+//
+//  Created by Mark Anthony Corpuz on 3/1/18.
+//  Copyright © 2018 SplashFactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
